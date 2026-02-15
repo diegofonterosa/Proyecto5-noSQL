e073f1c5d1c07f86e1183b6941e305a29a9a3cfb
diego <diegofonterosa@gmail.com>
2026-02-15 22:29:50 +0100

    revisión 15-02-26

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
-const express = require("express");
-const mongoose = require("mongoose")
-require("dotenv").config();
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const movieRoutes = require("./src/routes/movie.routes")
-const cinemaRoutes = require("./src/routes/cinema.routes")
+import movieRoutes from "./src/routes/movie.routes.js";
+import cinemaRoutes from "./src/routes/cinema.routes.js";
+
+dotenv.config();
 
 const server = express();
-
 server.use(express.json());
 
 server.use("/api/movies", movieRoutes);
 server.use("/api/cinemas", cinemaRoutes);
 
-mongoose.connect(process.env.MONGO_URI)
+const PORT = process.env.PORT || 8080;
+const MONGO_URI = process.env.MONGO_URI
+
+if (!MONGO_URI) {
+    console.error("MONGO_URI no está definida en el archivo .env");
+    process.exit(1);
+}
+
+mongoose.connect(MONGO_URI)
     .then(() => {
-        console.log("Conectado a la base de datos");
-        server.listen(8080, () => {
-            console.log("Servidor escuchando en http://localhost:8080");
+        console.log("Conectado a MongoDB");
+        server.listen(PORT, () => {
+            console.log(`Servidor escuchando en http://localhost:${PORT}`);
         });
     })
     .catch((error) => {
